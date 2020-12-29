59b167beb994eb79e86b1a0908d619ae566f28fa
mateo <mateofabiancruz@gmail.com>
2020-12-29 16:37:53 -0500

    add new files

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+let buttonNav = document.querySelector(".nav-button")
+let nav = document.querySelector(".nav")
+let body = document.querySelector("body")
+
+buttonNav.addEventListener('click',()=>{
+    nav.classList.toggle("nav-hidden")
+
+})
